b6db26015400a3de24a634d848d4efb9f31e2bab
Michael Perez <mperez6473@gmail.com>
2020-04-24 00:26:25 -0500

    Changed the order in which the functions run. Should push to homepage now and refresh it.

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import Axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 const MovieCard = (props) => {
     const { title, director, metascore, stars } = props.movie;
     const { state, setState } = useState(props.movie);
+    const { push } = useHistory();
+
     return (
         <div className="movie-card">
             <h2>{title}</h2>
@@ -24,12 +27,14 @@ const MovieCard = (props) => {
             <button
                 onClick={(e) => {
                     e.preventDefault();
-                    window.location.reload();
                     console.log(props.movie);
                     Axios.delete(
                         `http://localhost:5000/api/movies/${props.movie.id}`
                     )
                         .then((res) => {
+                            push('/');
+                            window.location.reload();
+
                             return res.data;
                             // res.data;
                         })
