b2aad89dbb4848bde6c17672146430005e51b312
Michael Perez <mperez6473@gmail.com>
2020-05-20 13:39:24 -0500

    Added the Route in App.js and added an edit button in Movie. Made a updateMovieForm.jsx

---
+++ client/src/Movies/updateMovieForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const updateMovieForm = () => {
+    return (
+        <section>
+            <h1>This is the update movie form area</h1>
+        </section>
+    );
+};
+export default updateMovieForm;
